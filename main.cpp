77829298bffbcea7e9c07596816b976e9be0bb02
Réthalmi Dávid <david.rethalmi@gmail.com>
2018-07-03 18:56:11 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    qDebug() << QT_VERSION_STR;
+
+    return a.exec();
+}
